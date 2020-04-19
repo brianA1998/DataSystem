659f105e77cb83aad51306cacffd0e611642457d
Brian <brian.alaniz@hotmail.com>
2020-04-19 01:12:18 -0300

    Agregando funcionalidad al boton acceder. Y creando las interfaces de Administrador,Capturista y Tecnico

--- src/ventanas/Login.java
+++ src/ventanas/Login.java
@@ -9,12 +9,18 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import java.sql.*;
+import clases.Conexion;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author GARBARINO
  */
 public class Login extends javax.swing.JFrame {
+
+    public static String user = "";
+    String pass = "";
 
     /**
      * Creates new form Login
@@ -29,27 +35,24 @@ public class Login extends javax.swing.JFrame {
         //Agregar la imagen dentro del JFrame
         ImageIcon wallpaper = new ImageIcon("src/images/wallpaperPrincipal.jpg");
         Icon icono = new ImageIcon(wallpaper.getImage().getScaledInstance(jLabel_Wallpaper.getWidth(),
-                jLabel_Wallpaper.getHeight(),Image.SCALE_DEFAULT));
-        
+                jLabel_Wallpaper.getHeight(), Image.SCALE_DEFAULT));
+
         jLabel_Wallpaper.setIcon(icono);
         this.repaint();
-        
-        
+
         ImageIcon wallpaper_logo = new ImageIcon("src/images/DS.png");
         Icon icono_logo = new ImageIcon(wallpaper_logo.getImage().getScaledInstance(jLabel_Logo.getWidth(),
-                jLabel_Logo.getHeight(),Image.SCALE_DEFAULT));
+                jLabel_Logo.getHeight(), Image.SCALE_DEFAULT));
         jLabel_Logo.setIcon(icono_logo);
         this.repaint();
-        
 
     }
+
     @Override
-    public Image getIconImage(){
+    public Image getIconImage() {
         Image retValue = Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("images/icon.png"));
         return retValue;
     }
-    
-    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -63,6 +66,8 @@ public class Login extends javax.swing.JFrame {
         jLabel_Logo = new javax.swing.JLabel();
         txt_user = new javax.swing.JTextField();
         txt_password = new javax.swing.JPasswordField();
+        jButton_Acceder = new javax.swing.JButton();
+        jLabel_Footer = new javax.swing.JLabel();
         jLabel_Wallpaper = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -83,10 +88,38 @@ public class Login extends javax.swing.JFrame {
         txt_password.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         txt_password.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         getContentPane().add(txt_password, new org.netbeans.lib.awtextra.AbsoluteConstraints(95, 370, 210, -1));
+
+        jButton_Acceder.setBackground(new java.awt.Color(153, 153, 255));
+        jButton_Acceder.setFont(new java.awt.Font("Arial Narrow", 0, 18)); // NOI18N
+        jButton_Acceder.setForeground(new java.awt.Color(255, 255, 255));
+        jButton_Acceder.setText("Acceder");
+        jButton_Acceder.setBorder(null);
+        jButton_Acceder.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_AccederActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton_Acceder, new org.netbeans.lib.awtextra.AbsoluteConstraints(95, 420, 210, 35));
+
+        jLabel_Footer.setText("Creado por brian ");
+        getContentPane().add(jLabel_Footer, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 520, -1, -1));
         getContentPane().add(jLabel_Wallpaper, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 400, 550));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton_AccederActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_AccederActionPerformed
+        user = txt_user.getText().trim();
+        pass = txt_password.getText().trim();
+
+        //Validacion de campos
+        if (!user.equals("") || !pass.equals("")) {
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Debes llenar todos los campos");
+        }
+
+    }//GEN-LAST:event_jButton_AccederActionPerformed
 
     /**
      * @param args the command line arguments
@@ -124,6 +157,8 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton_Acceder;
+    private javax.swing.JLabel jLabel_Footer;
     private javax.swing.JLabel jLabel_Logo;
     private javax.swing.JLabel jLabel_Wallpaper;
     private javax.swing.JPasswordField txt_password;
